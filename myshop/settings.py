3b5a2cf38e94592bb2708ef0d536470c4cf96fab
beko2021 <80588339+beko2021@users.noreply.github.com>
2021-03-14 13:50:58 +0100

    tow commit

--- myshop/settings.py
+++ myshop/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '#f__!oxad+gea*c9%-f_py+%we)!cl7178&=mh2yi1t*6w-ute'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kandaka.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
