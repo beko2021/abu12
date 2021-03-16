3e30ed7c237088af67382ca7e9dc28ecb42c650d
beko2021 <80588339+beko2021@users.noreply.github.com>
2021-03-16 23:40:53 +0100

    Update settings.py

--- myshop/settings.py
+++ myshop/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url
+ 
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,4 +131,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
-django_heroku.settings(locals())
+ 
